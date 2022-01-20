f0b31e2364b42e93f1774fc6c8ec7b3dc01f4bf2
GasanbekovRoman <spamerl256@gmail.com>
2022-01-20 20:55:46 +0400

    Initial Commit

---
+++ HomeWork3.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeWork3.1
+//
+//  Created by Asya  on 20.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HomeWork3.1/HomeWork3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork3_1App.swift
+//  HomeWork3.1
+//
+//  Created by Asya  on 20.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
